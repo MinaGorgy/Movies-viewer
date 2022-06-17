60581ac29f8d5be179c42caec4f3dab5594d2afd
Mina Morcos <minamorcosgorgy@gmail.com>
2022-06-17 14:43:07 +0200

    karma fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,5 +5,5 @@ export const environment = {
   apiKey: '3fa4fbacaa2e7abb78ef691e344b6c3b',
   searchMovies: 'search/movie',
   imagesConfigUrl: 'https://image.tmdb.org/t/p/w500',
-  getMovieDetails: 'movie'
+  getMovieDetails: 'movie/'
 };
